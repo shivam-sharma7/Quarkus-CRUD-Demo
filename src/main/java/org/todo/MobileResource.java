e16fa1bee01350343a3059ec68b96c58be4b49cb
Shivam Sharma <meshivam81@gmail.com>
2023-05-13 11:14:23 +0530

    add: CRUD operation successful

--- src/main/java/org/todo/MobileResource.java
+++ src/main/java/org/todo/MobileResource.java
@@ -8,15 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*Created an array list to store mobiles list */
+
 @Path("/mobile")
 public class MobileResource {
      List<String> mobileList = new ArrayList<>();
+
+
+    /*Created a Get method to show mobile list  */
 
      @GET
      @Produces(MediaType.TEXT_PLAIN)
      public Response getMobile(){
          return Response.ok(mobileList).build();
      }
+
+    /*Created a POST method to add new mobile  */
 
      @POST
      @Consumes(MediaType.TEXT_PLAIN)
@@ -26,12 +33,14 @@ public class MobileResource {
          return Response.ok(mobileName).build();
      }
 
+    /*Created a PUT method to update old mobile names in list with new mobile name  */
+
     @PUT
-    @Path("/{oldmobilename}?{newmobilename}")
+    @Path("/{oldmobilename}")
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
     public Response updateMobile(@PathParam("oldmobilename") String oldMobileName, @QueryParam("newmobilename") String newMobileName) {
-        mobileList.stream().map(mobile -> {
+        mobileList = mobileList.stream().map(mobile -> {
             if(mobile.equals(oldMobileName)){
                 return newMobileName;
 
@@ -43,4 +52,20 @@ public class MobileResource {
 
         return Response.ok(mobileList).build();
     }
+
+    /*Created a DELETE method to delete a mobile from  list  */
+
+   @DELETE
+   @Path("{mobileToDelete}")
+   @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteMobile(@PathParam("mobileToDelete") String mobileName){
+
+        boolean isRemoved =  mobileList.remove(mobileName);
+        if(isRemoved){
+            return Response.noContent().build();
+        }else{
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
 }
