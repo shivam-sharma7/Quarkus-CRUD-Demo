0a3691d79d85574ee13d3cfb2cf817e675861436
Shivam Sharma <meshivam81@gmail.com>
2023-05-16 21:13:54 +0530

    add new rest api with json response

---
+++ src/main/java/org/todo/Mobile.java
@@ -0,0 +1,11 @@
+package org.todo;
+
+public class Mobile {
+
+    int id;
+    String name;
+    String brand;
+    int ram;
+    int storage;
+
+}
--- src/main/java/org/todo/MobileResource.java
+++ src/main/java/org/todo/MobileResource.java
@@ -1,71 +1,33 @@
 package org.todo;
 
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-/*Created an array list to store mobiles list */
+/*Created an array list to store mobiles list and his properties */
 
 @Path("/mobile")
 public class MobileResource {
-     List<String> mobileList = new ArrayList<>();
+    List<Mobile> mobileList = new ArrayList<>();
 
-
-    /*Created a Get method to show mobile list  */
-
-     @GET
-     @Produces(MediaType.TEXT_PLAIN)
-     public Response getMobile(){
-         return Response.ok(mobileList).build();
-     }
-
-    /*Created a POST method to add new mobile  */
-
-     @POST
-     @Consumes(MediaType.TEXT_PLAIN)
-     @Produces(MediaType.TEXT_PLAIN)
-     public Response addNewMobile(String mobileName){
-         mobileList.add(mobileName);
-         return Response.ok(mobileName).build();
-     }
-
-    /*Created a PUT method to update old mobile names in list with new mobile name  */
-
-    @PUT
-    @Path("/{oldmobilename}")
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response updateMobile(@PathParam("oldmobilename") String oldMobileName, @QueryParam("newmobilename") String newMobileName) {
-        mobileList = mobileList.stream().map(mobile -> {
-            if(mobile.equals(oldMobileName)){
-                return newMobileName;
-
-            } else{
-                return mobile;
-            }
-
-        }).collect(Collectors.toList());
-
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMobileList(){
         return Response.ok(mobileList).build();
     }
 
-    /*Created a DELETE method to delete a mobile from  list  */
+    @POST
+    public Response createMobile(Mobile newMobile){
+        mobileList.add(newMobile);
+        return Response.ok(newMobile).build();
 
-   @DELETE
-   @Path("{mobileToDelete}")
-   @Produces(MediaType.TEXT_PLAIN)
-    public Response deleteMobile(@PathParam("mobileToDelete") String mobileName){
+     }
 
-        boolean isRemoved =  mobileList.remove(mobileName);
-        if(isRemoved){
-            return Response.noContent().build();
-        }else{
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-    }
 
 }
